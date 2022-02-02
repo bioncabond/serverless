2de99f4d8f1d4a5f84d27663268e4b4e9bf90525
Bionca Bond <bioncab@zillowgroup.com>
2022-02-02 12:35:40 -0800

    roger's define file

--- api/define.py
+++ api/define.py
@@ -10,24 +10,24 @@ class handler(BaseHTTPRequestHandler):
         
         #take the path and split it up into different components (see docs for more info) 
         url_components = parse.urlsplit(url_path) 
-        query_string_list = parse.parse_qsl(url_components) 
+        query_string_list = parse.parse_qsl(url_components.query) 
         dic = dict(query_string_list) 
 
         #grab a specific word from the api we are calling from 
         #EXAMPLE: https://serverless-khaki-alpha.vercel.app/api/define?word=python 
         #find if I asked for a word; look in the query string and see if word is in there (word = python)
 
-        if "word" in dic: 
-            #url w/o the seach key then we concat the thing we are looking for 
-            url = 'http://apiurl'  
-            r = request.get(url + dic['word'])
-            #set that json payload we are looking at to a variable (data)
-            data = r.json() 
-            definitions = []  
-            #there is a for loop here look at you notes from class 30
-        
-        else: 
-            message = "Please give me a word to define."
+        if "word" in dic:
+            url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+            r = requests.get(url + dic['word'])
+            data = r.json()
+            definitions = []
+            for word_data in data:
+                definition = word_data["meanings"][0]["definitions"][0]["definition"]
+                definitions.append(definition)
+            message = str(definitions)        
+        else:
+            message = "Please give me a word to define"
         
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
--- api/food.py
+++ api/food.py
@@ -15,42 +15,33 @@ class handler(BaseHTTPRequestHandler):
         #EXAMPLE: https://serverless-khaki-alpha.vercel.app/api/define?word=python 
         #find if I asked for a word; look in the query string and see if word is in there (word = python)
 
-#
-        # if "food" in dic: 
-        #     #url w/o the search key then we concat the thing we are looking for 
-        #     # url = 'https://https://api.spoonacular.com/recipes/716429/information?apiKey={api_key}'
-        #     url = 'https://api.spoonacular.com/information?apiKey={api_key}&food/search?food=apple'  
-        #     r = requests.get(url)
+
+        if "food" in dic: 
+            #url w/o the search key then we concat the thing we are looking for 
+            # url = 'https://https://api.spoonacular.com/recipes/716429/information?apiKey={api_key}'
+            url = 'https://api.spoonacular.com/information?apiKey={api_key}&food/search?food=apple'  
+            r = requests.get(url)
 
 
-        #     #set that json payload we are looking at to a variable (data)
-        #     data = r.json()
-        #     print(f"{data}") 
-        #     definitions = []  
-        #     for food_data in data:
-        #         definition = food_data["searchResults"][0]
-        #         definitions.append(definition)
-        #     message = str(definitions)     
-        
-        # else: 
-        #     message = "Please give me a food to get for you."
-        if "word" in dic:
-            url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
-            r = requests.get(url + dic['word'])
+            #set that json payload we are looking at to a variable (data)
             data = r.json()
-            definitions = []
-            for word_data in data:
-                definition = word_data["meanings"][0]["definitions"][0]["definition"]
+            print(f"{data}") 
+            definitions = []  
+            for food_data in data:
+                definition = food_data["searchResults"][0]
                 definitions.append(definition)
-            message = str(definitions)        
-        else:
-            message = "Please give me a word to define"
+            message = str(definitions)     
+        
+        else: 
+            message = "Please give me a food to get for you."
+
         
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers() 
 
         self.wfile.write(message.encode())
+
         return 
 
 
