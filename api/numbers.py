34f3f41ffdf399e1a0151ae4d946c64ee54fe1c7
Bionca Bond <bioncab@zillowgroup.com>
2022-02-02 19:35:18 -0800

    text

--- api/numbers.py
+++ api/numbers.py
@@ -24,7 +24,7 @@ class handler(BaseHTTPRequestHandler):
             num_facts = []  
 
             for num_data in data:
-                fact = num_data[0::][1::]
+                fact = num_data["text"]
                 num_facts.append(fact)
                 print("num facts:", num_facts)
             message = str(num_facts)     
