6f060a0fa453ee4119fa05960aa03a115349f5d4
Bionca Bond <bioncab@zillowgroup.com>
2022-02-02 20:25:51 -0800

    proof

--- api/numbers.py
+++ api/numbers.py
@@ -23,12 +23,12 @@ class handler(BaseHTTPRequestHandler):
             data = req.json()
             num_facts = []  
 
-            # for num_data in data:
-            #     print("1")
-                # fact = data.get('text')
+            fact = data.get('text')
+            # number_fact = data.get('number')
+
                 # num_facts.append(fact)
                 # print("num facts:", num_facts)
-            message = data   
+            message = str(fact)
         else: 
             message = "PLEASE PICK A NUMBER."
 
